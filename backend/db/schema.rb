1da1eb1d1723c25f2319678e92bcf54244c97fea
Eswath <eswath@codingmart.com>
2018-08-07 12:57:46 +0530

    Products Markup and login Using Access Token Rails

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_05_094913) do
+ActiveRecord::Schema.define(version: 2018_08_07_071639) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -36,6 +36,12 @@ ActiveRecord::Schema.define(version: 2018_07_05_094913) do
   create_table "company_assets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.integer "count"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
