30546e8e454a3b9677c903c6ba0e067f3b3b3dd8
Eswath <eswath@codingmart.com>
2018-08-08 15:22:52 +0530

    Bidding api fix

--- backend/app/controllers/biddings_controller.rb
+++ backend/app/controllers/biddings_controller.rb
@@ -53,6 +53,6 @@ class BiddingsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def bidding_params
-      params.require(:bidding).permit(:from_date, :to_date, :days, :markup)
+      params.require(:bidding).permit(:from_date, :to_date, :days, :markup, :user_id, :product_id)
     end
 end
