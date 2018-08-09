d88cf91287dcd7f540f502084550ee21da26f0fb
aravindmv97 <aravindmv97@gmail.com>
2018-08-09 19:50:17 +0530

    Deafult value for status in bidding table

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_09_095453) do
+ActiveRecord::Schema.define(version: 2018_08_09_141817) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 2018_08_09_095453) do
     t.datetime "updated_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
     t.bigint "user_id"
     t.bigint "product_id"
-    t.integer "status", default: 0, null: false
+    t.integer "status", default: 0
     t.index ["product_id"], name: "index_biddings_on_product_id"
     t.index ["user_id"], name: "index_biddings_on_user_id"
   end
