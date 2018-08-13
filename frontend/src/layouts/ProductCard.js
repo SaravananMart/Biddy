ba3e46902eb76dc0d5bffbb00e6651c36d1fe7f2
SaravananMart <r.saravanan@codingmart.com>
2018-08-13 11:19:41 +0530

    Product Card

--- frontend/src/layouts/ProductCard.js
+++ frontend/src/layouts/ProductCard.js
@@ -4,7 +4,7 @@ import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-// import Button from '@material-ui/core/Button';
+import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import image from '../images/shirt.jpeg'
 const styles = {
@@ -40,28 +40,15 @@ function ProductCard(props) {
                     <Typography gutterBottom variant="headline" component="h2">
                         {product.name}
                     </Typography>
-                    <Typography component="p">
-                        {product.description}
-                    </Typography>
-                    {/*<Typography>*/}
-                        {/*{product.productCount!==0 && <span>*/}
-                        {/*<span className='discountedPrice'>Rs {product.discountedPrice}</span>*/}
-                        {/*<span className="price">Rs {product.productPrice}</span>*/}
-                            {/*<span className='discount'>{product.productDiscount}% OFF</span>*/}
-                        {/*</span>*/}
-                        {/*}*/}
-
-                        {/*{product.productCount===0 && <span className='comingSoon'>Coming Soon</span>}*/}
-                    {/*</Typography>*/}
                 </CardContent>
 
                 <CardActions>
-                    {/*<Button size="small" color="primary">*/}
-                        {/*Share*/}
-                    {/*</Button>*/}
-                    {/*<Button size="small" color="primary">*/}
-                        {/*Learn More*/}
-                    {/*</Button>*/}
+                    <Button size="small" color="primary">
+                        Share
+                    </Button>
+                    <Button size="small" color="primary">
+                        Learn More
+                    </Button>
                 </CardActions>
             </Card>
         </div>
